97e15d787dd009d3b724a5295ab387e74a4b7bfd
Kolterg <mark.reta99@gmail.com>
2021-05-16 01:28:45 +0300

    Create branch HW2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 1
+// Взяти масиви об'єктів і створити їм компоненти, проітерувати і повиводити їх, повидаляти.
+
+// 2
+// Взяти масив юзерів створити кожному кнопку.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
